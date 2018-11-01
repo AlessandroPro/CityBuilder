00930af5f43a64e1f41321761f6955ec2ff26f5c
AlessandroPro <alessandro.profenna@gmail.com>
2018-11-01 11:37:42 -0400

    First commit

---
+++ CityBuilder/CityBuilder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CityBuilder
+//
+//  Created by Alessandro Profenna on 2018-11-01.
+//  Copyright © 2018 Alessandro Profenna. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
